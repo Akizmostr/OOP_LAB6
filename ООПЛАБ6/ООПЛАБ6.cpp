71722df3d2f133b9f862ebd897e47f889faa45d4
Слава <nterehina666@mail.ru>
2020-10-27 18:12:24 +0300

    good

--- ООПЛАБ6/ООПЛАБ6.cpp
+++ ООПЛАБ6/ООПЛАБ6.cpp
@@ -188,6 +188,24 @@ TreeNode<T*>::TreeNode(T*& new_data) {
 	right = NULL;
 }
 
+template<>
+TreeNode<char*>::TreeNode(char*& new_data) {
+	int length = 0;
+	while (new_data[length] != '\0')
+		length++;
+	length++;
+
+	data = new char[length];
+
+	strcpy_s(data, length, new_data);
+
+	/*for (int i = 0; i < length; i++)
+		data[i] = new_data[i];*/
+
+	left = NULL;
+	right = NULL;
+}
+
 template <typename T>
 TreeNode<T>::~TreeNode() {
 }
@@ -215,30 +233,32 @@ void TreeNode<T*>::printInfix() {
 	}
 }
 
+template<>
+void TreeNode<char*>::printInfix() {
+	if (this) {
+		this->left->printInfix();
+		std::cout << this->data << "\t";
+		this->right->printInfix();
+	}
+}
+
 int main()
 {
 	BinaryTree <char> char_tree;
 	BinaryTree <double> double_tree;
 	BinaryTree <std::string> string_tree;
-	BinaryTree <int*> intptr_tree;
 	char arr[7] = {'d', 'f', 'e', 'a', '^', 'W', 'q'};
-	double arr2[7] = { 2.5, 6, 0.6, 1, 9, 7 };
-	std::string arr3[3] = { "Hello world1", "Hello world3", "Hello world2" };
+	double arr1[7] = { 2.5, 6, 0.6, 1, 9, 7 };
+	std::string arr2[3] = { "Hello world1", "Hello world3", "Hello world2" };
 	for (auto var : arr) {
 		char_tree.insert(var);
 	}
-	for (auto var : arr2) {
+	for (auto var : arr1) {
 		double_tree.insert(var);
 	}
-	for (auto var : arr3) {
+	for (auto var : arr2) {
 		string_tree.insert(var);
 	}
-
-	int* xptr = new int{0};
-	int* yptr = new int{-4};
-	intptr_tree.insert(xptr);
-	intptr_tree.insert(yptr);
-
 
 	char_tree.printTreeInfix();
 	std::cout << std::endl;
@@ -246,15 +266,34 @@ int main()
 	std::cout << std::endl;
 	string_tree.printTreeInfix();
 	std::cout << std::endl;
+
+	BinaryTree <int*> intptr_tree;
+	int* xptr = new int{0};
+	int* yptr = new int{-4};
+	intptr_tree.insert(xptr);
+	intptr_tree.insert(yptr);
 	*xptr = -10;
 	intptr_tree.printTreeInfix();
 	std::cout << *xptr << "\t" << *yptr << std::endl;
 	delete xptr;
 	delete yptr;
 
-	char* str = new char[6]{"Hello"};
 	BinaryTree<char*> charptr_tree;
+	char* str2 = new char[5]{ "lokk" };
+	char* str = new char[12]{"Hello world"};
 	charptr_tree.insert(str);
+	charptr_tree.insert(str2);
 	charptr_tree.printTreeInfix();
+	std::cout << (*str2 < *str) << std::endl;
 	delete str;
+	delete str2;
+
+	BinaryTree <double*> doubleptrArray_tree;
+	double* arr3 = new double[3]{ 4.2, 2.3, 9 };
+	double* arr4 = new double[3]{ 4.1, 2.9, 10 };
+	doubleptrArray_tree.insert(arr3);
+	doubleptrArray_tree.insert(arr4);
+	doubleptrArray_tree.printTreeInfix();
+	delete arr3;
+	delete arr4;
 }
